6dedd77f1fbd3cb254a09eae31571e2cd6697e76
iansayre <me@iansayre.com>
2015-09-06 23:03:22 -0700

    updated the router in app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,26 @@
 'use strict';
 
-var westfieldApp = angular.module('westfieldApp', [
+var westfieldApp = angular.module( 'westfieldApp', [
 	'ngRoute',
 	'ui.bootstrap'
-]);
+] );
+
+
+westfieldApp.config( [ '$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when( '/tabs', {
+			templateURL: 'views/tabs.html',
+			controller: 'stateTabsCtrl'
+		} ).
+		when( '/accordion', {
+			templateURL: 'views/accordion.html',
+			controller: 'locationAccordionCtrl'
+		} ).
+		when( '/fallback', {
+			templateURL: 'views/fallback.html',
+			controller: 'fallbackCtrl'
+		} ).
+		otherwise( {
+			redirectTo: '/fallback'
+		} );
+} ] );
